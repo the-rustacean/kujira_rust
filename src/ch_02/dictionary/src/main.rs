983a5bef6c52222f35880e50b6e92d38a19e8d2b
YoungPhil <youngphil.gim@gmail.com>
2025-03-12 11:39:19 +0900

    add dictionary

--- src/ch_02/dictionary/src/main.rs
+++ src/ch_02/dictionary/src/main.rs
@@ -1,3 +1,28 @@
+use std::env;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::process;
+
 fn main() {
-    println!("Hello, world!");
+    let dic_file = "dict.txt";
+
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 2 {
+        eprintln!("[USAGE] .dictionary word");
+        process::exit(1);
+    }
+
+    let word = &args[1];
+
+    let fp = File::open(dic_file).unwrap();
+
+    let reader = BufReader::new(fp);
+
+    for line in reader.lines() {
+        let line = line.unwrap();
+        if let Some(_) = line.find(word) {
+            println!("{}", line);
+        }
+    }
 }
