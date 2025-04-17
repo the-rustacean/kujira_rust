23537517793ad580269cf11a56ef8452b39e9d8f
YoungPhil <youngphil.gim@gmail.com>
2025-04-17 15:12:20 +0900

    add iter_prime

---
+++ src/ch_04/iter_prime/src/main.rs
@@ -0,0 +1,43 @@
+fn main() {
+    let prime_iter = PrimeIterator::new();
+
+    for n in prime_iter {
+        print!("{}, ", n);
+    }
+}
+
+struct PrimeIterator {
+    n: u8,
+}
+
+impl PrimeIterator {
+    fn new() -> Self {
+        Self { n: 1 }
+    }
+    fn is_prime(&self) -> bool {
+        for i in 2..self.n {
+            if self.n % i == 0 {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+impl Iterator for PrimeIterator {
+    type Item = u8;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        loop {
+            self.n += 1;
+
+            if std::u8::MAX == self.n {
+                return None;
+            }
+
+            if self.is_prime() {
+                return Some(self.n);
+            }
+        }
+    }
+}
